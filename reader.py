7b421ebb820d4c74dc69bbb83f644c4e8dbcfb8a
Raghuttam Hombal <group4pgs@gmail.com>
2020-01-28 18:53:19 +0530

    Manjaro pc

---
+++ reader.py
@@ -0,0 +1,27 @@
+import serial
+import pyttsx3
+import time
+
+#Instal packages - serial, pyttsx3
+
+
+
+def sayIt(p):
+	print("-----------Starting to speak------------")	
+	engine=pyttsx3.init()
+	engine.say(p)
+	engine.runAndWait()	
+
+def serialObtain():
+	ser = serial.Serial('/dev/ttyACM0', 9600) #port of ARDUINO and also baud rate
+	while True :
+		try:
+			state=serial.readline()
+			return state
+		except:
+        	pass
+
+while True:
+	time.sleep(1)
+	sayIt(serialObtain)
+	
